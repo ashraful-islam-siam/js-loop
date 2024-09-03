7328e57b5edcbefed57d6d556188a435c6c0f9ca
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-09-03 11:27:12 +0600

    Added for loop task

---
+++ for-odd-even.js
@@ -0,0 +1,13 @@
+//Even
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log("Even:", i);
+  }
+}
+
+// Odd
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 1) {
+    console.log("Odd:", i);
+  }
+}
